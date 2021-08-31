b9de618a87e4dfcc3af5a01fe33118c346147d04
michaeljleo <80555730+michaeljleo@users.noreply.github.com>
2021-08-31 19:10:16 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Michael Leo on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
