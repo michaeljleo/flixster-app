032219b062ee0e3e2b0474d9ff4e63b13e4328f3
michaeljleo <80555730+michaeljleo@users.noreply.github.com>
2021-09-09 17:44:38 -0400

    Added a details menu and collection view

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Michael Leo on 9/9/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
